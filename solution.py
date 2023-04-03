23a1049b8e49a3c4ad842e940fabf3e92960715f
Agrant9 <96260635+Agrant9@users.noreply.github.com>
2023-04-03 16:30:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,4 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return np.log(np.mean(x)) - (np.log((np.std(x))**2/(np.mean(x))**2 + 1))**2/2 # Ваш ответ
+    return 459+np.log(np.mean(x)) - (np.log((np.std(x))**2/(np.mean(x))**2 + 1))**2/2 # Ваш ответ
